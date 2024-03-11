ec182111753102b17a25034c55de558754f6531e
megaslip <gleb.zyravlev98@gmail.com>
2024-03-11 13:46:22 +0300

    new

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "3ISP9-26VB\n";
+    std::cout << "Denisov, Zhuravlev\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
